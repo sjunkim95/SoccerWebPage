b092142d7c5c33942841a0ef3373b1797356e4a7
sjunk <sjunk@LAPTOP-9D48H40D>
2022-11-28 21:38:37 -0800

    group project

--- src/main/java/com/example/spring03/web/ViewController.java
+++ src/main/java/com/example/spring03/web/ViewController.java
@@ -1,5 +1,21 @@
 package com.example.spring03.web;
 
-public class ViewController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/view")
+public class ViewController {
+    
+    @GetMapping
+    public String main(){
+        log.info("main()");
+
+        return "main";
+    }
 }
+
