82c03e7c183daaf9cec986e724389dae03d8c56e
sjunk <sjunk@LAPTOP-9D48H40D>
2022-11-28 23:11:42 -0800

    group project

---
+++ src/main/java/com/example/spring03/page/Test.java
@@ -0,0 +1,47 @@
+//package com.example.spring03.page;
+//
+//import java.io.BufferedWriter;
+//import java.io.File;
+//import java.io.FileOutputStream;
+//import java.io.IOException;
+//import java.io.OutputStreamWriter;
+//
+//import org.jsoup.Jsoup;
+//import org.jsoup.nodes.Document;
+//import org.jsoup.nodes.Element;
+//import org.jsoup.select.Elements;
+//
+//public class Test {
+//
+//    public static void main(String[] args) {
+//
+//        try {
+//            Document doc = Jsoup.connect("http://rss.donga.com/sportsdonga/soccer.xml").get();
+//            Elements items = doc.select("item");
+//            
+//            File writeFile = new File("src/main/resources/templates/data/news.html");
+//            FileOutputStream fos = new FileOutputStream(writeFile);
+//            OutputStreamWriter osw = new OutputStreamWriter(fos, "UTF-8");
+//            BufferedWriter bw = new BufferedWriter(osw);
+//            
+//            for (Element item : items) {
+//                String title = item.selectFirst("title").text();
+//                String href = item.selectFirst("link").text();
+//                
+//                String tag = "<a hre='" + href + "'>" + title + "</a><br>";
+//                bw.write(tag);
+//                bw.newLine();
+//                
+//            }
+//            
+//            bw.flush();
+//            bw.close();
+//            System.out.println("성공");
+//        } catch (IOException e) {
+//            System.out.println("실패");
+//            e.printStackTrace();
+//        }
+//        
+//    }
+//
+//}
